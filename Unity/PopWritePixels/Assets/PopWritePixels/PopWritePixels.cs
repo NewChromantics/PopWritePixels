77dd52e5ecee605dc84be8ca5c8518d24cfd7c81
Graham Reeves <graham@grahamreeves.com>
2019-04-12 13:48:21 +0100

    Updated unity project & wrote new psuedo code

---
+++ Unity/PopWritePixels/Assets/PopWritePixels/PopWritePixels.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using System.Collections;					// required for Coroutines
+using System.Runtime.InteropServices;		// required for DllImport
+using System;								// requred for IntPtr
+using System.Text;
+using System.Collections.Generic;
+
+
+/// <summary>
+///	Low level interface
+/// </summary>
+public static class PopWritePixels 
+{
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+	private const string PluginName = "PopWritePixels";
+#else
+#error Unsupported platform
+#endif
+	
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern int AllocCacheTexture2D(IntPtr TexturePtr, int Width, int Height, TextureFormat PixelFormat);
+
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern void ReleaseCache(int Cache);
+
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern IntPtr GetWritePixelsWithCacheFunc();
+
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern bool SetWritePixelsBytes(int Cache, byte[] ByteData, int ByteDataSize);
+
+	[DllImport(PluginName, CallingConvention = CallingConvention.Cdecl)]
+	private static extern bool HasCacheWrittenBytes(int Cache);
+
+
+
+
+	public class JobCache
+	{
+		int?		CacheIndex = null;
+		IntPtr		TexturePtr;
+		IntPtr		PluginFunction;
+		Camera.CameraCallback IssueEventCallback = null;
+
+		public JobCache(Texture2D texture)
+		{
+			TexturePtr = texture.GetNativeTexturePtr();
+			//	gr: replace format with channels?
+			CacheIndex = AllocCacheTexture2D( TexturePtr, texture.width, texture.height, texture.format );
+			if ( CacheIndex == -1 )
+				throw new System.Exception("Failed to allocate cache index");
+
+			PluginFunction = GetWritePixelsWithCacheFunc();
+		}
+
+		~JobCache()
+		{
+			Release();
+		}
+
+		public void QueueWrite(byte[] Bytes,bool Copy=false,Camera AfterCamera=null)
+		{
+			//	todo: make copy of bytes here, but it'll be slow
+			//			we can pin/get GC handle for byte[] but intptr won't work that way
+			if (Copy)
+				throw new System.Exception("Currently not copying, assuming caller won't delete bytes over the next frame");
+
+			if (!SetWritePixelsBytes(CacheIndex.Value, Bytes, Bytes.Length))
+				throw new System.Exception("SetCacheBytes returned error");
+
+			//	queue a write
+			if (AfterCamera != null) {
+				if (IssueEventCallback == null) {
+					IssueEventCallback = (c) => {
+						if ( c == AfterCamera )
+							GL.IssuePluginEvent (PluginFunction, CacheIndex.Value);
+					};
+					Camera.onPostRender += IssueEventCallback;
+				}
+			} else {
+				GL.IssuePluginEvent (PluginFunction, CacheIndex.Value);
+			}
+		}
+		
+		public bool	HasFinished()
+		{
+			var Written = HasCacheWrittenBytes(CacheIndex.Value);
+			return Written;
+		}
+
+		public void	Release()
+		{
+			//	gr: check we don't release whilst still using data
+			if (IssueEventCallback != null)
+			{
+				Camera.onPostRender -= IssueEventCallback;
+				IssueEventCallback = null;
+			}
+
+			if ( CacheIndex.HasValue )
+				ReleaseCache( CacheIndex.Value );
+		}
+	}
+
+	public static JobCache WritePixelsAsync(Texture texture,byte[] Pixels,Camera AfterCamera=null)
+	{
+		Debug.Log ("allocating");
+		/*
+		if ( texture is RenderTexture )
+		{
+			var Job = new JobCache( texture as RenderTexture );
+			Job.QueueWrite(Pixels,AfterCamera);
+			return Job;
+		}
+		*/
+		if ( texture is Texture2D )
+		{
+			var Job = new JobCache( texture as Texture2D );
+			Job.QueueWrite(Pixels,AfterCamera);
+			return Job;
+		}
+
+		throw new System.Exception("Texture type not handled");
+	}
+	
+	
+
+}
---
+++ Unity/PopWritePixels/Assets/PopWritePixels_Demo/CreateTextureAndApplyToMaterial.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTextureAndApplyToMaterial : MonoBehaviour {
+
+	public int TextureWidth = 1024;
+	public int TextureHeight = 1024;
+	public FilterMode TextureFilterMode = FilterMode.Point;
+	public TextureFormat TextureFormat = TextureFormat.RGBA32;
+	public bool MipMap = false;
+	public List<Color> WriteColours = new List<Color>(new Color[]{Color.red, Color.green, Color.blue});
+	public Texture2D NewTexture;
+	PopWritePixels.JobCache WritePixelsJob;
+	byte[] PixelBytes;
+
+	void OnEnable()
+	{
+		//	make new texture
+		NewTexture = new Texture2D(TextureWidth, TextureHeight, TextureFormat, MipMap);
+		NewTexture.filterMode = TextureFilterMode;
+		var Mat = this.GetComponent<MeshRenderer>().material;
+		Mat.mainTexture = NewTexture;
+
+		//	generate pixels
+		if (WriteColours.Count == 0)
+			WriteColours.Add(Color.cyan);
+		var PixelColours = new Color32[TextureWidth * TextureHeight];
+		PixelBytes = new byte[TextureWidth*TextureHeight*4];
+		for (int y = 0; y < TextureHeight; y++)
+		{
+			var Colour = WriteColours[y % WriteColours.Count];
+			var Colour32 = new Color32((byte)(Colour.r * 255), (byte)(Colour.g * 255), (byte)(Colour.b * 255), (byte)(Colour.a * 255) );
+			for (int x = 0; x < TextureWidth; x++)
+			{
+				var PixelIndex = (x + (y * TextureWidth)) * 4;
+				PixelBytes[PixelIndex + 0] = Colour32.r;
+				PixelBytes[PixelIndex + 0] = Colour32.g;
+				PixelBytes[PixelIndex + 0] = Colour32.b;
+				PixelBytes[PixelIndex + 0] = Colour32.a;
+				PixelColours[x + (y * TextureWidth)] = Colour32;
+			}
+		}
+		if ( NewTexture.mipmapCount == 0 )
+			NewTexture.LoadRawTextureData(PixelBytes);
+		else
+			NewTexture.SetPixels32(PixelColours);
+		NewTexture.Apply();
+		WritePixelsJob = PopWritePixels.WritePixelsAsync(NewTexture, PixelBytes);
+	}
+
+	void Update()
+	{
+		if (WritePixelsJob==null)
+		{
+			throw new System.Exception("No write job");
+		}
+
+		if (WritePixelsJob.HasFinished())
+		{
+			Debug.Log("Finished writing pixels");
+			this.enabled = false;
+			WritePixelsJob.Release();
+		}
+	}
+}
